33761ca6f2de586e944fec6399d19f06b09d55b8
Nello Carotenuto <a.carotenuto16@studenti.unisa.it>
2017-12-13 12:38:49 +0100

    Inizializzazione progetto

---
+++ webapp/src/main/java/it/unisa/di/tirociniosmart/webapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package it.unisa.di.tirociniosmart.webapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebappApplication.class);
+	}
+
+}
