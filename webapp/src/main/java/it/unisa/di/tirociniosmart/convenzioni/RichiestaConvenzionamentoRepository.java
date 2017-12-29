446003c3fbcdd3f496ca892115b7b52052e81256
Rosanna Coccaro <rosannacoccaro.rc@gmail.com>
2017-12-29 17:49:06 +0100

    RichiestaConvenzionamentoRepository: permette di definire le operazioni per l'interrogazione e la manipolazione dei dati relativamente alle Richieste di convenzionamento salvate nel database

---
+++ webapp/src/main/java/it/unisa/di/tirociniosmart/convenzioni/RichiestaConvenzionamentoRepository.java
@@ -0,0 +1,67 @@
+package it.unisa.di.tirociniosmart.convenzioni;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Classe che definisce le operazioni relative alla persistenza delle richieste di convenzionamento.
+ * 
+ * @see RichiestaConvenzionamento
+ */
+
+@Repository
+public interface RichiestaConvenzionamentoRepository extends 
+    JpaRepository<RichiestaConvenzionamento, Long> {
+  
+  /**
+   * Permette di ottenere l'elenco delle richieste di convenzionamento salvate nel Database.
+   * 
+   * @return List<RichiestaConvenzionamento> che rappresenta la lista delle richieste 
+   *                                         di convenzionamento
+   */
+  List<RichiestaConvenzionamento> findAll();
+  
+  /**
+   * Permette di ottenere una richiesta di convenzionamento a partire dal suo identificatore.
+   * 
+   * @param id numero di tipo long che rappresenta l'identificativo della richiesta di
+   *           convenzionamento
+   *           
+   * @return RichiestaConvenzionamento che rappresenta la richiesta di convenzionamento.
+   * 
+   * @pre id > 0
+   */
+  RichiestaConvenzionamento findById(long id);
+  
+  /**
+   * Permette di ottenere l'elenco delle richieste di convenzionamento con un determinato status.
+   * 
+   * @param status Intero che rappresenta lo stato assegnato alla richiesta di
+   *               convenzionamento
+   *           
+   * @return List<RichiestaConvenzionamento> che rappresenta la lista delle richieste 
+   *                                         di convenzionamento
+   *                                         
+   * @pre status = {@link RichiestaConvenzionamento#IN_ATTESA} or
+   *      status = {@link RichiestaConvenzionamento#APPROVATA} or
+   *      status = {@link RichiestaConvenzionamento#RIFIUTATA}
+   */
+  List<RichiestaConvenzionamento> findAllByStatus(int status);
+  
+  /**
+   * Permette di verificare se una richiesta di convenzionamento esiste nel database attraverso il 
+   * proprio identificatore.
+   * 
+   * @param id numero di tipo long che rappresenta l'identificativo della richiesta di
+   *            convenzionamento
+   *               
+   * @return true se la richiesta di convenzionamento esiste,
+   *         false se la richiesta di convenzionamento non esiste
+   *         
+   * @pre id > 0
+   */
+  boolean existsById(long id);
+  
+
+}
