a0bd3473f6735b24bc38381b814d66428c4c3302
NelloCarotenuto <a.carotenuto16@studenti.unisa.it>
2018-01-12 02:23:39 +0100

    Test: AziendaRepository: completa la suite per il testing dei metodi

--- webapp/src/test/java/it/unisa/di/tirociniosmart/convenzioni/AziendaRepositoryIT.java
+++ webapp/src/test/java/it/unisa/di/tirociniosmart/convenzioni/AziendaRepositoryIT.java
@@ -1,8 +1,18 @@
 package it.unisa.di.tirociniosmart.convenzioni;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.everyItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsIn.isIn;
+import static org.junit.Assert.assertThat;
+
 import it.unisa.di.tirociniosmart.utenza.UtenteRegistrato;
 
-import org.junit.Assert;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -27,34 +37,200 @@ public class AziendaRepositoryIT {
   @Autowired
   private AziendaRepository aziendaRepository;
   
+  private static List<Azienda> listaAziende;
+  
   /**
-   * Testa l'interazione con il database per l'inserimento ed il successivo caricamento
-   * dell'azienda (tramite identificatore).
+   * Popola la lista {@link #listaAziende} con oggetti fittizi che faranno da sorgente di dati per
+   * le operazioni di lettura e scrittura su database.
+   */
+  @BeforeClass
+  public static void inizializzaAziende() {
+    listaAziende = new ArrayList<Azienda>();
+    
+    
+    // Crea l'azienda #1 ed inseriscila in lista
+    Azienda azienda1 = new Azienda();
+    azienda1.setId("acmeltd");
+    azienda1.setNome("ACME Ltd.");
+    azienda1.setPartitaIva("01234567890");
+    azienda1.setSenzaBarriere(true);
+    
+    DelegatoAziendale delegato1 = azienda1.getDelegato();
+    delegato1.setUsername("wilee");
+    delegato1.setPassword("beepbeep");
+    delegato1.setEmail("wilee@coyote.com");
+    delegato1.setNome("Wile E.");
+    delegato1.setCognome("Coyote");
+    delegato1.setSesso(UtenteRegistrato.SESSO_MASCHILE);
+    delegato1.setTelefono("9876543210");
+    
+    listaAziende.add(azienda1);
+    
+    
+    // Crea l'azienda #2 ed inseriscila in lista
+    Azienda azienda2 = new Azienda();
+    azienda2.setId("starkind");
+    azienda2.setNome("Stark Industries");
+    azienda2.setPartitaIva("74598763241");
+    azienda2.setSenzaBarriere(true);
+    
+    DelegatoAziendale delegato2 = azienda2.getDelegato();
+    delegato2.setUsername("tonystark");
+    delegato2.setPassword("ironman");
+    delegato2.setEmail("tony@starkind.com");
+    delegato2.setNome("Anthony Edward");
+    delegato2.setCognome("Stark");
+    delegato2.setSesso(UtenteRegistrato.SESSO_MASCHILE);
+    delegato2.setTelefono("7485214786");
+    
+    listaAziende.add(azienda2);
+    
+    
+    // Crea l'azienda #3 ed inseriscila in lista
+    Azienda azienda3 = new Azienda();
+    azienda3.setId("cyberdynecorp");
+    azienda3.setNome("Cyberdyne System Corporation");
+    azienda3.setPartitaIva("54569814752");
+    azienda3.setSenzaBarriere(false);
+    
+    DelegatoAziendale delegato3 = azienda3.getDelegato();
+    delegato3.setUsername("milesdyson");
+    delegato3.setPassword("terminator");
+    delegato3.setEmail("miles@cyberdyne.net");
+    delegato3.setNome("Miles");
+    delegato3.setCognome("Dyson");
+    delegato3.setSesso(UtenteRegistrato.SESSO_MASCHILE);
+    delegato3.setTelefono("7451453658");
+    
+    listaAziende.add(azienda3);
+  }
+  
+  /**
+   * Salva la lista delle aziende su database prima dell'esecuzione di ogni singolo test.
+   * @throws InterruptedException 
+   */
+  @Before
+  public void salvaAziende() throws InterruptedException {
+    for (Azienda azienda : listaAziende) {
+      aziendaRepository.save(azienda);
+    }
+  }
+  
+  /**
+   * Testa l'interazione con il database per l'inserimento ed il successivo caricamento di una lista
+   * di aziende.
+   * 
+   * @test {@link AziendaRepository#findAll()}
+   * 
+   * @result Il test è superato se tutte le aziende salvate sono correttamente caricate
+   */
+  @Test
+  public void findAll() {
+    // Controlla che ogni elemento della lista restituita dalla repository sia presente nella lista
+    // utilizzata per il test e viceversa
+    List<Azienda> listaAziendeSalvate = aziendaRepository.findAll();
+    assertThat(listaAziende, everyItem(isIn(listaAziendeSalvate)));
+    assertThat(listaAziendeSalvate, everyItem(isIn(listaAziende)));
+  }
+  
+  /**
+   * Testa l'interazione con il database per il singolo caricamento delle aziende della lista
+   * tramite identificatore.
    * 
    * @test {@link AziendaRepository#findById(String)}
    * 
-   * @result Il test è superato se l'entità viene correttamente salvata e caricata dal database
+   * @result Il test è superato se ogni entità viene correttamente caricata dal database
    */
   @Test
-  public void findByIdTest() {
-    Azienda azienda = new Azienda();
-    azienda.setId("acmeltd");
-    azienda.setNome("ACME Ltd.");
-    azienda.setPartitaIva("01234567890");
-    azienda.setSenzaBarriere(true);
+  public void findById() {
+    // Controlla che ogni azienda della lista per il test sia presente su database ricercandola per
+    // l'identificatore
+    for (Azienda azienda : listaAziende) {
+      Azienda aziendaSalvata = aziendaRepository.findById(azienda.getId());
+      assertThat(azienda, is(equalTo(aziendaSalvata)));
+    }
+  }
+  
+  /**
+   * Testa l'interazione con il database per il caricamento della lista di aziende che non hanno
+   * barriere architettoniche.
+   * 
+   * @test {@link AziendaRepository#findAllBySenzaBarriere(boolean)
+   * 
+   * @result Il test è superato se sono caricate solo le aziende della lista che non hanno barriere
+   *         architettoniche
+   */
+  @Test
+  public void findAllBySenzaBarriere() {
+    // Definisci la lista delle aziende senza barriere a partire da quelle usate per il test
+    List<Azienda> listaAziendeSenzaBarriere = new ArrayList<Azienda>();
+    for (Azienda azienda : listaAziende) {
+      if (azienda.isSenzaBarriere()) {
+        listaAziendeSenzaBarriere.add(azienda);
+      }
+    }
     
-    DelegatoAziendale delegato = azienda.getDelegato();
-    delegato.setUsername("wilee");
-    delegato.setEmail("wilee@coyote.com");
-    delegato.setNome("Wile E.");
-    delegato.setCognome("Coyote");
-    delegato.setPassword("beepbeep");
-    delegato.setSesso(UtenteRegistrato.SESSO_MASCHILE);
-    delegato.setTelefono("9876543210");
+    // Controlla che ogni elemento della lista restituita dalla repository sia nella lista
+    // utilizzata per il test
+    List<Azienda> listaAziendeSenzaBarriereSalvate = aziendaRepository.findAllBySenzaBarriere(true);
+    assertThat(listaAziendeSenzaBarriereSalvate, everyItem(isIn(listaAziendeSenzaBarriere)));
+    assertThat(listaAziendeSenzaBarriere, everyItem(isIn(listaAziendeSenzaBarriereSalvate)));
+  }
+  
+  /**
+   * Testa l'interazione con il database per determinare se la ricerca di un'azienda tramite partita
+   * IVA avvenga correttamente.
+   * 
+   * @test {@link AziendaRepository#existsByPartitaIva(String)}
+   * 
+   * @result Il test è superato se la ricerca delle partite IVA delle aziende presenti nella lista
+   *         utilizzata per il test ha successo e se la ricerca di una partita IVA vuota o
+   *         inesistente non ha successo
+   */
+  @Test
+  public void existsByPartitaIva() {
+    // Controlla che ogni azienda della lista utilizzata per il test sia presente su database 
+    // ricercandola per partita IVA
+    for (Azienda azienda : listaAziende) {
+      boolean aziendaEsistente = aziendaRepository.existsByPartitaIva(azienda.getPartitaIva());
+      assertThat(aziendaEsistente, is(true));
+    }
     
-    aziendaRepository.save(azienda);
+    // Controlla che non esistono aziende con partita IVA vuota
+    boolean aziendaSenzaPartitaIvaEsistente = aziendaRepository.existsByPartitaIva("");
+    assertThat(aziendaSenzaPartitaIvaEsistente, is(false));
     
-    Assert.assertEquals(azienda, aziendaRepository.findById(azienda.getId()));
+    // Controlla che non esistono aziende con partita IVA inesistente
+    boolean aziendaPartitaIvaFintaEsistente = aziendaRepository.existsByPartitaIva("11111111111");
+    assertThat(aziendaPartitaIvaFintaEsistente, is(false));
+  }
+  
+  /**
+   * Testa l'interazione con il database per determinare se la ricerca di un'azienda tramite
+   * identificatore avvenga correttamente.
+   * 
+   * @test {@link AziendaRepository#existsById(String)}
+   * 
+   * @result Il test è superato se la ricerca degli identificatori delle aziende presenti nella
+   *         lista utilizzata per il test ha successo e se la ricerca di una partita IVA vuota o
+   *         inesistente non ha successo
+   */
+  @Test
+  public void existsById() {
+    // Controlla che ogni azienda della lista utilizzata per il test sia presente su database 
+    // ricercandola per id
+    for (Azienda azienda : listaAziende) {
+      boolean aziendaEsistente = aziendaRepository.existsById(azienda.getId());
+      assertThat(aziendaEsistente, is(true));
+    }
+    
+    // Controlla che non esistono aziende con id vuoto
+    boolean aziendaSenzaPartitaIvaEsistente = aziendaRepository.existsById("");
+    assertThat(aziendaSenzaPartitaIvaEsistente, is(false));
+    
+    // Controlla che non esistono aziende con id inesistente
+    boolean aziendaPartitaIvaFintaEsistente = aziendaRepository.existsById("11111111111");
+    assertThat(aziendaPartitaIvaFintaEsistente, is(false));
   }
   
 }
