055c8edaf85525499cf298cfe6670b974f483c64
NelloCarotenuto <a.carotenuto16@studenti.unisa.it>
2018-01-13 16:49:27 +0100

    Spring: definisci classe per la configurazione della webapp

---
+++ webapp/src/main/java/it/unisa/di/tirociniosmart/WebConfig.java
@@ -0,0 +1,9 @@
+package it.unisa.di.tirociniosmart;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
