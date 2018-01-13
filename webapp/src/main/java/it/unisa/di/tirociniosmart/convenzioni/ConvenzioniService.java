682dc67d8f025658518415168fc1904c788707f7
NelloCarotenuto <a.carotenuto16@studenti.unisa.it>
2018-01-13 20:39:05 +0100

    Convenzioni: Service: Ristruttura API per la validazione

--- webapp/src/main/java/it/unisa/di/tirociniosmart/convenzioni/ConvenzioniService.java
+++ webapp/src/main/java/it/unisa/di/tirociniosmart/convenzioni/ConvenzioniService.java
@@ -1,16 +1,7 @@
 package it.unisa.di.tirociniosmart.convenzioni;
 
-import it.unisa.di.tirociniosmart.utenza.CognomeNonValidoException;
-import it.unisa.di.tirociniosmart.utenza.EmailEsistenteException;
-import it.unisa.di.tirociniosmart.utenza.EmailNonValidaException;
-import it.unisa.di.tirociniosmart.utenza.NomeNonValidoException;
-import it.unisa.di.tirociniosmart.utenza.PasswordNonValidaException;
-import it.unisa.di.tirociniosmart.utenza.SessoNonValidoException;
-import it.unisa.di.tirociniosmart.utenza.TelefonoNonValidoException;
-import it.unisa.di.tirociniosmart.utenza.UsernameEsistenteException;
-import it.unisa.di.tirociniosmart.utenza.UsernameNonValidoException;
 import it.unisa.di.tirociniosmart.utenza.UtenteRegistrato;
-import it.unisa.di.tirociniosmart.utenza.UtenteRegistratoService;
+import it.unisa.di.tirociniosmart.utenza.UtenzaService;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -37,7 +28,7 @@ public class ConvenzioniService {
   private RichiestaConvenzionamentoRepository richiestaConvenzionamentoRepository;
   
   @Autowired
-  private UtenteRegistratoService utenteService;
+  private UtenzaService utenzaService;
   
   /**
    * Permette di richiedere al sistema il salvataggio di un'azienda. La procedura registra l'azienda
@@ -49,162 +40,31 @@ public class ConvenzioniService {
    *                questo metodo.
    * 
    * @pre azienda != null
-   * 
-   * @throws IdAziendaNonValidoException se l'identificatore dell'azienda è null o non rispetta il
-   *         formato definito in {@link Azienda#ID_PATTERN}
-   * 
-   * @throws IdAziendaEsistenteException se l'identificatore dell'azienda è già presente nel sistema
-   * 
-   * @throws PartitaIvaAziendaNonValidaException se la partita IVA dell'azienda è nulla o non
-   *         rispetta il formato definito in {@link Azienda#PARTITA_IVA_PATTERN}
-   * 
-   * @throws PartitaIvaAziendaEsistenteException se la partita IVA dell'azienda è già presente nel
-   *         sistema
-   * 
-   * @throws NomeAziendaNonValidoException se il nome dell'azienda è nullo o se la sua lunghezza
-   *         non rientra nell'intervallo che va dal valore di {@link Azienda#MIN_LUNGHEZZA_NOME} a
-   *         quello di {@link Azienda#MAX_LUNGHEZZA_NOME}
-   * 
-   * @throws IndirizzoAziendaNonValidoException se l'indirizzo dell'azienda è nullo o se la sua
-   *         lunghezza non rientra nell'intervallo che va da {@link Azienda#MIN_LUNGHEZZA_INDIRIZZO}
-   *         a {@link Azienda#MAX_LUNGHEZZA_INDIRIZZO}
-   * 
-   * @throws DelegatoNonValidoException se il delegato dell'azienda è nullo
-   * 
-   * @throws UsernameNonValidoException se l'username del delegato non è specificato oppure se non
-   *         rispetta il formato {@link UtenteRegistrato#USERNAME_PATTERN}
-   * 
-   * @throws UsernameEsistenteException se l'username del delegato è già presente nel sistema
-   * 
-   * @throws PasswordNonValidaException se la password del delegato non è specificata oppure se non
-   *         rispetta il formato {@link UtenteRegistrato#PASSWORD_PATTERN}
-   * 
-   * @throws EmailNonValidaException se l'e-mail del delegato non è specificata oppure se non
-   *         rispetta il formato {@link UtenteRegistrato#EMAIL_PATTERN}
-   * 
-   * @throws EmailEsistenteException se l'e-mail del delegato è gia presente nel sistema
-   * 
-   * @throws NomeNonValidoException se il nome del delegato è nullo oppure se la sua lunghezza non
-   *         rientra nell'intervallo che va da {@link UtenteRegistrato#MIN_LUNGHEZZA_NOME} a
-   *         {@link UtenteRegistrato#MAX_LUNGHEZZA_NOME}
-   * 
-   * @throws CognomeNonValidoException se il cognome del delegato è nullo oppure se la sua lunghezza
-   *         on rientra nell'intervallo che va da {@link UtenteRegistrato#MIN_LUNGHEZZA_NOME} a
-   *         {@link UtenteRegistrato#MAX_LUNGHEZZA_NOME}
-   *         
-   * @throws SessoNonValidoException se il sesso del delegato non è una delle costanti
-   *         {@link UtenteRegistrato#SESSO_MASCHILE} e {@link UtenteRegistrato#SESSO_FEMMINILE}
-   *         
-   * @throws TelefonoNonValidoException se il numero di telefono del delegato non è specificato o
-   *         non rispetta il formato definito in {@link UtenteRegistrato#TELEFONO_PATTERN}
    */
   @Transactional
-  public void registraRichiestaConvenzionamento(Azienda azienda)
-         throws PartitaIvaAziendaNonValidaException, IdAziendaNonValidoException,
-                IdAziendaEsistenteException, PartitaIvaAziendaEsistenteException,
-                NomeAziendaNonValidoException, IndirizzoAziendaNonValidoException,
-                DelegatoNonValidoException, UsernameNonValidoException, UsernameEsistenteException,
-                PasswordNonValidaException, EmailNonValidaException, EmailEsistenteException,
-                NomeNonValidoException, CognomeNonValidoException, SessoNonValidoException,
-                TelefonoNonValidoException {
-    // Controlla che l'id sia specificato, rispetti il formato, non sia già presente nel sistema
-    // ed assegnalo all'azienda (poiché ne è stato effettuato il trim)
-    String idAzienda = azienda.getId();
-    if (idAzienda == null) {
-      throw new IdAziendaNonValidoException();
-    } else {
-      idAzienda = idAzienda.trim();
-      
-      if (!idAzienda.matches(Azienda.ID_PATTERN)) {
-        throw new IdAziendaNonValidoException();
-      } else if (aziendaRepository.existsById(idAzienda)) {
-        throw new IdAziendaEsistenteException();
-      } else {
-        azienda.setId(idAzienda);
-      }
-    }
+  public void registraRichiestaConvenzionamento(Azienda azienda) throws Exception {
+    // Valida i campi dell'azienda
+    azienda.setId(validaIdAzienda(azienda.getId()));
+    azienda.setPartitaIva(validaPartitaIvaAzienda(azienda.getPartitaIva()));
+    azienda.setNome(validaNomeAzienda(azienda.getNome()));
+    azienda.setIndirizzo(validaIndirizzoAzienda(azienda.getIndirizzo()));
     
-    // Controlla che la partita IVA sia specificata, rispetti il formato, non sia già presente nel
-    // sistema ed assegnala all'azienda (poiché ne è stato effettuato il trim)
-    String partitaIvaAzienda = azienda.getPartitaIva();
-    if (partitaIvaAzienda == null) {
-      throw new PartitaIvaAziendaNonValidaException();
-    } else {
-      partitaIvaAzienda = partitaIvaAzienda.trim();
-      
-      if (!partitaIvaAzienda.matches(Azienda.PARTITA_IVA_PATTERN)) {
-        throw new PartitaIvaAziendaNonValidaException();
-      } else if (aziendaRepository.existsByPartitaIva(partitaIvaAzienda)) {
-        throw new PartitaIvaAziendaEsistenteException();
-      } else {
-        azienda.setPartitaIva(partitaIvaAzienda);
-      }
-    }
-    
-    // Controlla che il nome sia specificato, che la sua lunghezza rispetti i parametri prefissati
-    // ed assegnalo all'azienda (poiché ne è stato effettuato il trim)
-    String nomeAzienda = azienda.getNome();
-    if (nomeAzienda == null) {
-      throw new NomeAziendaNonValidoException();
-    } else {
-      nomeAzienda = nomeAzienda.trim();
-      
-      if (nomeAzienda.length() < Azienda.MIN_LUNGHEZZA_NOME
-          || nomeAzienda.length() > Azienda.MAX_LUNGHEZZA_NOME) {
-        throw new NomeAziendaNonValidoException();
-      } else {
-        azienda.setIndirizzo(nomeAzienda);
-      }
-    }
-    
-    // Controlla che l'indirizzo sia specificato, che la sua lunghezza rispetti i parametri
-    // prefissati ed assegnalo all'azienda (poiché ne è stato effettuato il trim)
-    String indirizzoAzienda = azienda.getIndirizzo();
-    if (indirizzoAzienda == null) {
-      throw new IndirizzoAziendaNonValidoException();
-    } else {
-      indirizzoAzienda = indirizzoAzienda.trim();
-      
-      if (indirizzoAzienda.length() < Azienda.MIN_LUNGHEZZA_INDIRIZZO
-          || indirizzoAzienda.length() > Azienda.MAX_LUNGHEZZA_INDIRIZZO) {
-        throw new IndirizzoAziendaNonValidoException();
-      } else {
-        azienda.setIndirizzo(indirizzoAzienda);
-      } 
-    }
-    
-    // Controlla che il delegato aziendale sia ben formato
+    // Valida i campi del delegato aziendale
     DelegatoAziendale delegato = azienda.getDelegato();
-    if (delegato == null) {
-      throw new DelegatoNonValidoException();
-    } else {
-      delegato = (DelegatoAziendale) utenteService.validaUtente(delegato);
-      
-      char sesso = delegato.getSesso();
-      if (sesso != UtenteRegistrato.SESSO_MASCHILE && sesso != UtenteRegistrato.SESSO_FEMMINILE) {
-        throw new SessoNonValidoException();
-      }
-      
-      String telefono = delegato.getTelefono();
-      if (telefono == null) {
-        throw new TelefonoNonValidoException();
-      } else {
-        telefono = telefono.trim();
-        
-        if (!telefono.matches(UtenteRegistrato.TELEFONO_PATTERN)) {
-          throw new TelefonoNonValidoException();
-        } else {
-          delegato.setTelefono(telefono);
-        }
-      }
-    }
+    delegato.setUsername(utenzaService.validaUsername(delegato.getUsername()));
+    delegato.setPassword(utenzaService.validaPassword(delegato.getPassword()));
+    delegato.setEmail(utenzaService.validaEmail(delegato.getEmail()));
+    delegato.setNome(utenzaService.validaNome(delegato.getNome()));
+    delegato.setCognome(utenzaService.validaCognome(delegato.getCognome()));
+    delegato.setTelefono(utenzaService.validaTelefono(delegato.getTelefono()));
+    delegato.setSesso(utenzaService.validaSesso(delegato.getSesso()));
     
     // Imposta stato e data della richiesta
     RichiestaConvenzionamento richiesta = azienda.getRichiesta();
     richiesta.setStatus(RichiestaConvenzionamento.IN_ATTESA);
     richiesta.setDataRichiesta(LocalDateTime.now());
     
-    // Registra la richiesta
+    // Registra le informazioni
     aziendaRepository.save(azienda);
   }
   
@@ -305,4 +165,125 @@ public class ConvenzioniService {
     return aziendaRepository.findAllByRichiestaConvenzionamentoStatus(
                                                                RichiestaConvenzionamento.APPROVATA);
   }
+  
+  /**
+   * Controlla che l'identificatore di un'azienda sia specificato e che rispetti il formato
+   * prestabilito. Controlla inoltre che tale identificatore non sia già presente nel sistema.
+   * 
+   * @param idAzienda Stringa che rappresenta l'identificatore da controllare
+   * 
+   * @return La stringa che rappresenta l'identificatore da controllare bonificata
+   * 
+   * @throws IdAziendaNonValidoException se l'identificatore passato come parametro è nullo oppure
+   *         se non rispetta il formato {@link Azienda#ID_PATTERN}
+   *         
+   * @throws IdAziendaEsistenteException se l'identificatore passato come parametro è già presente
+   *         nel sistema
+   */
+  public String validaIdAzienda(String idAzienda)
+         throws IdAziendaNonValidoException, IdAziendaEsistenteException {
+    // Controlla che il parametro non sia nullo, quindi effettua il trim e verifica che rispetti il
+    // formato prestabilito e che non sia presente nel database
+    if (idAzienda == null) {
+      throw new IdAziendaNonValidoException();
+    } else {
+      idAzienda = idAzienda.trim();
+      
+      if (!idAzienda.matches(Azienda.ID_PATTERN)) {
+        throw new IdAziendaNonValidoException();
+      } else if (aziendaRepository.existsById(idAzienda)) {
+        throw new IdAziendaEsistenteException();
+      } else {
+        return idAzienda;
+      }
+    }
+  }
+  
+  /**
+   * Controlla che il nome di un'azienda sia specificato e che la sua lunghezza rispetti i parametri
+   * prestabiliti.
+   * 
+   * @param nomeAzienda Stringa che rappresenta il nome da controllare
+   * 
+   * @return La stringa che rappresenta il nome da controllare bonificata
+   * 
+   * @throws NomeAziendaNonValidoException se il nome passato come parametro è nullo o se la sua
+   *         lunghezza non rientra nell'intervallo che va da
+   *         {@link UtenteRegistrato#MIN_LUNGHEZZA_NOME} a
+   *         {@link UtenteRegistrato#MAX_LUNGHEZZA_NOME}
+   */
+  public String validaNomeAzienda(String nomeAzienda) throws NomeAziendaNonValidoException {
+    if (nomeAzienda == null) {
+      throw new NomeAziendaNonValidoException();
+    } else {
+      nomeAzienda = nomeAzienda.trim();
+      
+      if (nomeAzienda.length() < Azienda.MIN_LUNGHEZZA_NOME
+          || nomeAzienda.length() > Azienda.MAX_LUNGHEZZA_NOME) {
+        throw new NomeAziendaNonValidoException();
+      } else {
+        return nomeAzienda;
+      }
+    }
+  }
+  
+  /**
+   * Controlla che la partita IVA di un'azienda sia specificata e che rispetti il formato
+   * prestabilito. Controlla inoltre che tale partita IVA non sia già presente nel sistema.
+   * 
+   * @param partitaIvaAzienda Stringa che rappresenta la partita iva da controllare
+   * 
+   * @return La stringa che rappresenta la partita IVA da controllare bonificata
+   * 
+   * @throws PartitaIvaAziendaNonValidaException se la partita IVA passata come parametro è nulla
+   *         oppure se non rispetta il formato {@link Azienda#PARTITA_IVA_PATTERN}
+   * 
+   * @throws PartitaIvaAziendaEsistenteException se la partita IVA passata come parametro è già
+   *         presente nel sistema
+   */
+  public String validaPartitaIvaAzienda(String partitaIvaAzienda)
+         throws PartitaIvaAziendaNonValidaException, PartitaIvaAziendaEsistenteException {
+    if (partitaIvaAzienda == null) {
+      throw new PartitaIvaAziendaNonValidaException();
+    } else {
+      partitaIvaAzienda = partitaIvaAzienda.trim();
+      
+      if (!partitaIvaAzienda.matches(Azienda.PARTITA_IVA_PATTERN)) {
+        throw new PartitaIvaAziendaNonValidaException();
+      } else if (aziendaRepository.existsByPartitaIva(partitaIvaAzienda)) {
+        throw new PartitaIvaAziendaEsistenteException();
+      } else {
+        return partitaIvaAzienda;
+      }
+    }
+  }
+  
+  /**
+   * Controlla che l'indirizzo di un'azienda sia specificato e che la sua lunghezza rispetti i
+   * parametri prestabiliti.
+   * 
+   * @param indirizzoAzienda Stringa che rappresenta l'indirizzo da controllare
+   * 
+   * @return La stringa che rappresenta l'indirizzo da controllare bonificata
+   * 
+   * @throws IndirizzoAziendaNonValidoException se l'indirizzo dell'azienda è nullo o se la sua
+   *         lunghezza non rientra nell'intervallo che va da {@link Azienda#MIN_LUNGHEZZA_INDIRIZZO}
+   *         a {@link Azienda#MAX_LUNGHEZZA_INDIRIZZO}
+   */
+  public String validaIndirizzoAzienda(String indirizzoAzienda)
+         throws IndirizzoAziendaNonValidoException {
+    if (indirizzoAzienda == null) {
+      throw new IndirizzoAziendaNonValidoException();
+    } else {
+      indirizzoAzienda = indirizzoAzienda.trim();
+      
+      if (indirizzoAzienda.length() < Azienda.MIN_LUNGHEZZA_INDIRIZZO
+          || indirizzoAzienda.length() > Azienda.MAX_LUNGHEZZA_INDIRIZZO) {
+        throw new IndirizzoAziendaNonValidoException();
+      } else {
+        return indirizzoAzienda;
+      } 
+    }
+  }
+  
 }
