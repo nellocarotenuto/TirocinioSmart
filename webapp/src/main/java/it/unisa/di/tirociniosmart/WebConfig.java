0542d4715a0245e5b64923da1e81c83e3081afab
NelloCarotenuto <a.carotenuto16@studenti.unisa.it>
2018-01-14 19:23:46 +0100

    Spring: risolvi richieste per le risorse statiche

--- webapp/src/main/java/it/unisa/di/tirociniosmart/WebConfig.java
+++ webapp/src/main/java/it/unisa/di/tirociniosmart/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -45,6 +46,16 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     resolver.setPrefix("/WEB-INF/views/");
     resolver.setSuffix(".jsp");
     return resolver;
+  }
+  
+  /**
+   * Mappa le richieste per le risorse statiche alla locazione appropriata.
+   */
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry
+        .addResourceHandler("/resources/**")
+        .addResourceLocations("/resources/"); 
   }
   
 }
