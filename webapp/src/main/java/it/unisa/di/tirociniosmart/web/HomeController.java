467287c2870d1b656a8713c122fc4d7b37d76d53
Nello Carotenuto <a.carotenuto16@studenti.unisa.it>
2018-01-17 00:09:26 +0100

    UI: introduci home page

---
+++ webapp/src/main/java/it/unisa/di/tirociniosmart/web/HomeController.java
@@ -0,0 +1,27 @@
+package it.unisa.di.tirociniosmart.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Controller che espone via web la home page del sito.
+ */
+@Controller
+public class HomeController {
+
+  /**
+   * Delega la vista dedicata alla presentazione della home page del sito.
+   * 
+   * @param model Contenitore degli attributi <b>iniettato dalla dispatcher servlet</b>
+   * 
+   * @return La stringa che rappresenta il nome della vista dedicata alla realizzazione della home
+   *         page
+   */
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String mostraHomePage(Model model) {
+    return "pages/home";
+  }
+  
+}
---
+++ webapp/src/main/webapp/WEB-INF/views/pages/home.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<spring:message var="titoloPagina" scope="request" code="pagina.home.titolo" />
+<spring:message var="testoNotifica" code="${testoNotifica}" />
+
+<jsp:include page="/WEB-INF/views/common/header.jsp">
+  <jsp:param name="titoloPagina" value="${titoloPagina}"/>
+</jsp:include>
+
+<jsp:include page="/WEB-INF/views/common/footer.jsp" />
