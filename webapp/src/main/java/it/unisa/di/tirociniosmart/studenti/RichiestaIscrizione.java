ce882e5429710a673ad7f2f12927a6b2fb0eaef3
Nello Carotenuto <a.carotenuto16@studenti.unisa.it>
2018-01-17 17:56:26 +0100

    RichiestaIscrizione: genera gli ID automaticamente

--- webapp/src/main/java/it/unisa/di/tirociniosmart/studenti/RichiestaIscrizione.java
+++ webapp/src/main/java/it/unisa/di/tirociniosmart/studenti/RichiestaIscrizione.java
@@ -3,6 +3,8 @@ package it.unisa.di.tirociniosmart.studenti;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
@@ -158,6 +160,7 @@ public class RichiestaIscrizione {
   
   
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
   private int status;
   private LocalDateTime dataRichiesta;
