829646ee67780fbc8c54862e1a670b691d5880ee
Nello Carotenuto <a.carotenuto16@studenti.unisa.it>
2018-01-17 16:40:31 +0100

    Autenticazione: Interceptor: rimuovi import inutilizzati

--- webapp/src/main/java/it/unisa/di/tirociniosmart/web/AutenticazioneInterceptor.java
+++ webapp/src/main/java/it/unisa/di/tirociniosmart/web/AutenticazioneInterceptor.java
@@ -7,9 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.ui.Model;
-import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
