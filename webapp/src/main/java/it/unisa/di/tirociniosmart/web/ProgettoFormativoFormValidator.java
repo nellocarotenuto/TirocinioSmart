c651029e8a22bfcd6961e4955b07c14262848065
Giuseppe Dello Stretto <peppe.dello.stretto97@gmail.com>
2018-01-20 19:51:42 +0100

    Progetto formativo: creazione form validator

---
+++ webapp/src/main/java/it/unisa/di/tirociniosmart/web/ProgettoFormativoFormValidator.java
@@ -0,0 +1,68 @@
+package it.unisa.di.tirociniosmart.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+
+import it.unisa.di.tirociniosmart.progettiformativi.DescrizioneNonValidaException;
+import it.unisa.di.tirociniosmart.progettiformativi.NomeProgettoNonValidoException;
+import it.unisa.di.tirociniosmart.progettiformativi.ProgettiFormativiService;
+
+
+
+/**
+ * Classe che definisce un validatore per {@link ProgettoFormativoForm} tramite i servizi offerti da
+ * {@link ProgettiFormativiService}. Il controllo viene effettuato su tutti i
+ * campi del form.
+ *
+ * @see ProgettoFormativoForm
+ * @see ProgettiFormativiService
+ * 
+ */
+
+@Component
+public class ProgettoFormativoFormValidator extends RegistrazioneFormValidator{
+  
+  @Autowired
+  private ProgettiFormativiService progettiFormativiService;
+  
+  
+  /**
+   * Permette di definire le classi cui il validatore è applicabile.
+   */
+  @Override
+  public boolean supports(Class<?> clazz) {
+    return ProgettoFormativoForm.class.isAssignableFrom(clazz);
+  }
+  
+  /**
+   * Effettua la validazione dell'oggetto target riportando gli errori
+   * nell'oggetto errors
+   * 
+   * @param target Oggetto da validare
+   * 
+   * @param errors Oggetto in cui salvare l'esito della validazione
+   */
+  @Override
+  public void validate(Object target, Errors errors) {
+    ProgettoFormativoForm form = (ProgettoFormativoForm) target;
+    
+    super.validate(target, errors);
+    
+    try {
+      progettiFormativiService.validaNome(form.getNome());
+    } catch (NomeProgettoNonValidoException e) {
+      errors.rejectValue("nomeProgettoFormativo", "toast.progettiFormativi.nomeNonValido");
+    } 
+    
+    try {
+      progettiFormativiService.validaDescrizione(form.getDescrizione());
+    } catch (DescrizioneNonValidaException e) {
+      errors.rejectValue("descrizioneProgettoFormativo", 
+                         "toast.progettiFormativi.descrizioneNonValida");
+    } 
+    
+  }
+  
+}
