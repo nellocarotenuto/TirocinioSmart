e2232a9aafcdeef272c8a4b6b494b2e49c13f7cb
Rosanna Coccaro <rosannacoccaro.rc@gmail.com>
2018-01-20 16:27:19 +0100

    ProgettoFormativo: Repository: aggiunta metodo existsById

--- webapp/src/main/java/it/unisa/di/tirociniosmart/progettiformativi/ProgettoFormativoRepository.java
+++ webapp/src/main/java/it/unisa/di/tirociniosmart/progettiformativi/ProgettoFormativoRepository.java
@@ -45,4 +45,17 @@ public interface ProgettoFormativoRepository extends JpaRepository<ProgettoForma
    * 
    */
   List<ProgettoFormativo> findAllByStatusAndAziendaId(int status, String idAzienda);
+  
+  /**
+   * Permette di verificare se un progetto formativo esiste nel database 
+   * attraverso il proprio identificatore.
+   * 
+   * @param id long che rappresenta l'identificativo del progetto formativo
+   *               
+   * @return true se il progetto esiste nel database,
+   *         false se il progetto non esiste nel database
+   *         
+   * @pre id > 0
+   */
+  boolean existsById(long id);
 }
