7149e2d5c8601cbc470d5b276b430d34e1986d39
Nello Carotenuto <a.carotenuto16@studenti.unisa.it>
2018-02-18 15:16:29 +0100

    Documentazione: pubblica documenti e build

--- docs/database/creazione.sql
+++
@@ -1,5 +0,0 @@
--- Script di inizializzazione da utilizzare SOLO prima dell'installazione della web app.
--- L'avvio dell'applicazione web non richiede l'esecuzione di questo script.
-
--- Creazione dello schema. Nome NON MODIFICABILE a meno di modifiche in application.properties della webapp
-CREATE SCHEMA tirociniosmart;
---
+++ docs/docs/database/inizializzazione.sql
@@ -0,0 +1,16 @@
+-- Script per l'inserimento di uno o più impiegati nel database da utilizzare solamente dopo aver eseguito creazione.sql
+-- ed aver avviato per la prima volta la webapp.
+
+USE tirociniosmart;
+
+-- Inserimento dell'impiegato dell'ufficio tirocini
+-- Specificare i valori dei campi username, password, email, nome e login da utilizzare per accedere all'account
+
+-- IMPORTANTE: il primo parametro inserito nella prima riga VALUES deve coincidere con l'unico parametro della seconda riga VALUES
+INSERT INTO utente_registrato (username, password, email, nome, cognome)
+	   VALUES ('ufficiotirocini', 'ufficiotirocini', 'tirocini@di.unisa.it', 'Ufficio Tirocini', 'Unisa');
+INSERT INTO impiegato_ufficio_tirocini (username)
+       VALUES ('ufficiotirocini');
+       
+-- Ripetere le righe superiori tante volte quant'è il numero degli impiegati da voler inserire
+-- IMPORTANTE: garantire l'unicità di username ed email
